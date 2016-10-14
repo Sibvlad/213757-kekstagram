b4538393046e31824fdf590260d6685f637ad674
Владислав Сибирев <sibvlad88@mail.ru>
2016-10-14 21:34:26 +0300

    Начало программирования

--- src/js/check.js
+++ src/js/check.js
@@ -1,32 +1,32 @@
 'use strict';
 
-function getMessage(a, b) {
-    if (typeof a === 'boolean') {
-       if (a === true) {
-        return 'Переданное GIF-изображение анимировано и содержит ' +  b + ' кадров'; 
-       }  if (a === false) {
-       return 'Переданное GIF-изображение не анимировано';
-       }
-    }
-    if (typeof a === 'number') {
-        return  'Переданное SVG-изображение содержит ' +  a + ' объектов и ' + b * 4 + ' атрибутов';
-    }
-    if (Array.isArray(a)) {
-        var amountOfRedPoints=0;
-        for (var i=0; i < a.length; i++) {
-           amountOfRedPoints+=a[i]; 
+var getMessage=function(a, b) {
+  if (typeof a === 'boolean') {
+        if (a === true) {
+         return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров'; 
+       } if (a === false) {
+         return 'Переданное GIF-изображение не анимировано';
         }
-        return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
     }
-    if (Array.isArray(a)&&Array.isArray(b)) {  
-          var artifactsSquare=0;
-          for(var i=0; i < a.length&&i<b.length; i++) {
-              artifactsSquare+=a[i]*b[i];
-          }
-          return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
-        }
-    else  {
-        console.log('Переданы некорректные данные');
+  if (typeof a === 'number') {
+      return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
     }
-} 
+   if (Array.isArray(a) && Array.isArray(b)) {
+        var artifactsSquare = 0;
+        for (var i = 0; i < a.length; i++) {
+        artifactsSquare += a[i] * b[i];
+    }
+    return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  } 
+  if (Array.isArray(a)) {
+      var amountOfRedPoints = a.reduce(function(sum, current){
+         return sum + current;                                
+       }, 0); 
+      return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+      }
+  else  {
+      console.log('Переданы некорректные данные');
+    }
+}
 
+  
