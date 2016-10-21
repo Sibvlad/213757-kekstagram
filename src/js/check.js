1878df9613ebccdf6edc5c9c97f2f6b71b268bb5
Владислав Сибирев <sibvlad88@mail.ru>
2016-10-22 01:16:53 +0300

    Последние изменения Canvas

--- src/js/check.js
+++ src/js/check.js
@@ -1,6 +1,6 @@
 'use strict';
-/* exported getMessage(a, b) */
-var getMessage = function(a, b) {
+
+function getMessage(a, b) {
   if (a === true) {
     return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
   } else if (a === false) {
@@ -15,14 +15,12 @@ var getMessage = function(a, b) {
       artifactsSquare += a[i] * b[i];
     }
     return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
-  }
-  else if (Array.isArray(a)) {
+  } else if (Array.isArray(a)) {
     var amountOfRedPoints = a.reduce(function(sum, current) {
       return sum + current;
     }, 0);
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
-  }
-  else {
+  } else {
     return 'Переданы некорректные данные';
   }
 }
--- src/js/resizer.js
+++ src/js/resizer.js
@@ -109,7 +109,7 @@
       // Отрисовка изображения на холсте. Параметры задают изображение, которое
       // нужно отрисовать и координаты его верхнего левого угла.
       // Координаты задаются от центра холста.
-      this._ctx.drawImage(this._image, displX, displY); 
+      this._ctx.drawImage(this._image, displX, displY);
       this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
       this._ctx.beginPath();
       this._ctx.rect(displX, displY, this._container.width, this._container.height);
@@ -118,7 +118,7 @@
         (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
         this._resizeConstraint.side + this._ctx.lineWidth / 2,
         this._resizeConstraint.side + this._ctx.lineWidth / 2);
-      this._ctx.fill('evenodd'); 
+      this._ctx.fill('evenodd');
       // Отрисовка прямоугольника, обозначающего область изображения после
       // кадрирования. Координаты задаются от центра.
       this._ctx.strokeRect(
@@ -126,6 +126,8 @@
         (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
       this._resizeConstraint.side - this._ctx.lineWidth / 2,
       this._resizeConstraint.side - this._ctx.lineWidth / 2);
+      //Вставляем текст над рамкой
+      this.drawText();
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
